6827763f1e9c7dec7acde21425079aadba9ccf50
alejandroq <alejandro.quintero@clouxter.com>
2022-02-14 21:21:43 -0500

    first

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,11 +1,13 @@
-const APIURL = "https://x79vukts55.execute-api.us-west-2.amazonaws.com/dev"
-const COGNITO_CLIENT_ID = '5otfktdju5vrvlti160dlv5te4'
-const POOL_ID1 = 'us-west-2_aOoLEZHI4'
+const APIURL = "https://3egzyk3mzj.execute-api.us-east-1.amazonaws.com/dev"
+const COGNITO_CLIENT_ID = '1o8m9no0s9mn1squb8f3g1kl6r'
+const COGNITO_POOL_ID = 'us-east-1_Fum8K3y6T'
+const COGNITO_POOL_URL = 'cognito-idp.us-east-1.amazonaws.com/us-east-1_Fum8K3y6T'
 // multiple pool parameters o0ms$0oZ.
 
 export default {
   APIURL: APIURL,
   COGNITO_CLIENT_ID: COGNITO_CLIENT_ID,
-  COGNITO_POOL_ID: POOL_ID1
+  COGNITO_POOL_ID: COGNITO_POOL_ID,
+  COGNITO_POOL_URL: COGNITO_POOL_URL
 
 }
--- src/components/login.vue
+++ src/components/login.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card id="tab">
       <b-card-title id="title">Login</b-card-title>
-      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form @submit="onSubmit" @reset="onReset" >
         <b-form-group
           class="userform"
           id="input-group-2"
@@ -42,151 +42,99 @@
         >
       </b-form>
 
-      <h2 v-if="show2">Change Password</h2>
-      <b-form @submit="onSubmit2" @reset="onReset2" v-if="show2">
-        <b-form-group
-          class="userform"
-          id="input-group-3"
-          label="a Username:"
-          label-for="input-3"
-        >
-          <b-form-input
-            id="input-3"
-            v-model="form2.username"
-            placeholder="Enter username"
-            class="userform"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          id="input-group-4"
-          label="password:"
-          label-for="input-4"
-          class="userform"
-          description="Enter new password"
-        >
-          <b-form-input
-            id="input-4"
-            v-model="form2.password"
-            type="password"
-            placeholder="Enter new password"
-            class="userform"
-            required
-          >
-          </b-form-input>
-
-          <b-form-group
-            id="input-group-5"
-            label="Session id:"
-            label-for="input-5"
-            class="userform"
-            description="Session id"
-          ></b-form-group>
-          <b-form-input
-            id="input-5"
-            v-model="form2.session"
-            placeholder="Enter new password"
-            class="userform"
-            disabled
-          ></b-form-input>
-        </b-form-group>
-
-
-        <p></p>
-        <b-button id="button1" type="submit" variant="outline-primary"
-          >Reset password</b-button
-        >
-      </b-form>
     </b-card>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import APIURL from "./constants.js";
-import COGNITO_POOL_ID from "./constants.js";
+import { CognitoUserPool, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
+import APIURL  from "./constants.js";
+import COGNITO_POOL_ID  from "./constants.js";
+import COGNITO_POOL_URL  from "./constants.js";
+import COGNITO_CLIENT_ID  from "./constants.js";
+import AWS from 'aws-sdk';
+
 export default {
   name: "login",
   data() {
     return {
       apiurl: APIURL["APIURL"],
+
+      userPoolUrl:COGNITO_POOL_URL['COGNITO_POOL_URL'],
+      userPool:null,
+      usePoolData:null,
       form: {
         password: "",
-        username: "",
-        pool: COGNITO_POOL_ID['POOL_ID1']
-      },
-      show: true,
-      show2: false,
-      form2: {
-        password: "",
-        username: "",
-        session: "",
-        pool: ""
-      },
+        username: ""
+      }
     };
   },
+  created(){
+    var poolData = {
+        UserPoolId:COGNITO_POOL_ID['COGNITO_POOL_ID'],
+        ClientId:COGNITO_CLIENT_ID['COGNITO_CLIENT_ID'],
 
+        }
+    this.usePoolData = poolData
+    this.userPool = new CognitoUserPool(poolData);
+  },
   methods: {
+
     onSubmit(event) {
       event.preventDefault();
       console.log(this.form)
-      axios.post(this.apiurl + "/login", this.form).then((response) => {
-        if (response["data"]["idToken"]) {
-          this.$router.push({
-            path: "dashboards",
-            query: { openIdToken: response["data"]["idToken"], 
-                     poolId: response["data"]["poolId"], 
-                     clientId: response["data"]["clientId"],
-                     domainUrl: response["data"]["domainUrl"]
-                     },
-          });
-        } else {
+      var authenticationData = {
+        Username: this.form.username,
+        Password: this.form.password
 
-            this.show2 = true;
-            this.show = false;
-            this.form2.username = this.form.username;
-            this.form2.session = response["data"]["Session"];
-            this.form2.pool = this.form.pool
-            
-          
-        }
-      });
-    },
-    onReset(event) {
-      event.preventDefault();
-      // Reset our form values
-      this.form.email = "";
-      this.form.username = "";
-      // Trick to reset/clear native browser form validation state
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
-    },
-    onSubmit2(event) {
-      event.preventDefault();
-      axios
-        .post(this.apiurl + "/login", this.form2)
-        .then((response) => console.log(response));
-          this.show2 = false;
+      }
+      var authenticationDetails = new AuthenticationDetails( authenticationData );
+      var userData = {
+        Username: this.form.username,
+        Pool: this.userPool
+        };
+      var cognitoUser = new CognitoUser(userData);
+      var _router = this.$router
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function(result) {
+          AWS.config.region = 'us-east-1';
+          AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'us-east-1:fe88ecb0-0eae-464a-9903-f7e828da0cda', 
+            Logins: {
+              'cognito-idp.us-east-1.amazonaws.com/us-east-1_Fum8K3y6T': result
+              .getIdToken()
+              .getJwtToken(),
+              },
+              });
+          AWS.config.credentials.refresh(error => {
+            if (error) {
+              console.error(error);
+              } else {
+                console.log('Successfully logged!');
+                _router.push({path:'dashboards'})
+
+                }
+                });
+              
+                },
+        onFailure: function(err) {
+          alert(err.message || JSON.stringify(err));
+          },
+          });
+          },
+        onReset(event) {
+          event.preventDefault();
+          // Reset our form values
+          this.form.email = "";
+          this.form.username = "";
+          // Trick to reset/clear native browser form validation state
+          this.show = false;
+          this.$nextTick(() => {
             this.show = true;
-            this.form.username = this.form2.username;
-    },
-    onReset2(event) {
-      event.preventDefault();
-      // Reset our form values
-      this.form.email = "";
-      this.form.username = "";
-      this.form.checked = [];
-      // Trick to reset/clear native browser form validation state
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
-    },
-  },
-};
+            });
+          }
+            },
+            };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -211,10 +159,5 @@ export default {
   width: 30%;
   left: 35%;
 }
-#button2 {
-  position: relative;
-  top: 55%;
-  width: 30%;
-  left: 35%;
-}
+
 </style>
